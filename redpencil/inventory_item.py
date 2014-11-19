88aed49c7a1349951c7c744b56296666a2ba9d5f
Raymond Chandler III <raymondchandleriii@gmail.com>
2014-11-19 15:38:00 -0500

    Refactored day counter to use a real time and date library.

--- redpencil/inventory_item.py
+++ redpencil/inventory_item.py
@@ -1,27 +1,40 @@
+from datetime import datetime
+
 class InventoryItem:
 
-    def __init__(self, price=0, days_last_changed=0):
+    def __init__(self, price=0, last_price_changed_on=None):
         self._price = price
-        self._days_since_price_changed = days_last_changed
         self._promotion_active = False
         self._days_promotion_active = 0
+
+        self._last_price_changed_on = last_price_changed_on
+
+        if not self._last_price_changed_on: 
+            self._last_price_changed_on = datetime.now()
+
+
+    def days_since_price_changed(self):
+        return (datetime.now() - self._last_price_changed_on).days
+
 
     def set_price(self, price):
         self._promotion_active = self.check_promotion(
             self._price, 
             price,
-            self._days_since_price_changed
+            self.days_since_price_changed()
         )
 
         self._price = price
-        self._days_since_price_changed = 0
+        self._last_price_changed_on = datetime.now()
+
+
+    def in_promotion(self):
+        return self._promotion_active
 
 
     def price_drop_ratio(self, old_price, new_price):
         return round(1 - (new_price / old_price), 2)
 
-    def in_promotion(self):
-        return self._promotion_active
 
     def check_promotion(self, old_price, new_price, days_last_changed):
         if old_price == 0:
--- tests/test_inventory_item.py
+++ tests/test_inventory_item.py
@@ -1,6 +1,8 @@
 import sure
 import unittest
 
+from datetime import datetime, timedelta
+
 from redpencil import InventoryItem
 
 class TestInventoryItem(unittest.TestCase):
@@ -14,17 +16,12 @@ class TestInventoryItem(unittest.TestCase):
     def test_it_should_not_be_in_promotion(self):
         self.subject.in_promotion().should.equal(False)
 
-    def test_it_should_have_a_day_counter_since_price_changed(self):
-        self.subject._days_since_price_changed.should.equal(0)
+    def test_it_should_set_a_default_last_price_changed_on(self):
+        self.subject._last_price_changed_on.should_not.be.none
 
     def test_it_should_start_in_a_non_promotional_state(self):
         self.subject._promotion_active.should.equal(False)
         self.subject._days_promotion_active.should.equal(0)
-
-    def test_it_should_let_us_set_price_and_duration_on_init(self):
-        self.other_subject = InventoryItem(25, 30)
-        self.other_subject._price.should.equal(25)
-        self.other_subject._days_since_price_changed.should.equal(30)
 
     def test_it_should_let_us_set_a_new_price(self):
         self.subject.set_price(25)
@@ -35,11 +32,16 @@ class TestInventoryItem(unittest.TestCase):
 class TestInventoryItemPriceChange(unittest.TestCase):
 
     def setUp(self):
-        self.subject = InventoryItem(100, 30)
+        self.last_price_changed_on = datetime.now() - timedelta(days=30)
+        self.subject = InventoryItem(100, self.last_price_changed_on)
+
+    def test_it_should_let_us_set_price_and_last_price_changed_on_init(self):
+        self.subject._price.should.equal(100)
+        self.subject._last_price_changed_on.should.equal(self.last_price_changed_on)
 
     def test_it_should_update_price_changed_when_price_is_set(self):
         self.subject.set_price(50)
-        self.subject._days_since_price_changed.should.equal(0)
+        self.subject._last_price_changed_on.should_not.equal(self.last_price_changed_on)
 
     def test_it_should_calculate_price_drop_ratio(self):
         self.subject.price_drop_ratio(100, 50).should.equal(0.5)
@@ -73,12 +75,12 @@ class TestInventoryItemPriceChange(unittest.TestCase):
         self.subject.in_promotion().should.equal(False)
 
     def test_it_should_not_enter_promotion_when_price_hasnt_remained_stable_for_30_days(self):
-        self.subject = InventoryItem(100, 29)
+        self.subject = InventoryItem(100, datetime.now() - timedelta(days=29))
         self.subject.set_price(99)
         self.subject.in_promotion().should.equal(False)
 
     def test_it_should_enter_promotion_when_price_reamined_stable_for_31_days(self):
-        self.subject = InventoryItem(100, 31)
+        self.subject = InventoryItem(100, datetime.now() - timedelta(days=31))
         self.subject.set_price(95)
         self.subject.in_promotion().should.equal(True)
 
