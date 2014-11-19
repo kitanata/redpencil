81d616e3963f16efdb135f2ad8ca8f4c29b985bb
Raymond Chandler III <raymondchandleriii@gmail.com>
2014-11-19 15:14:19 -0500

    Refactors the inventory item class to remove some state that isn't needed for calculations.

--- redpencil/inventory_item.py
+++ redpencil/inventory_item.py
@@ -1,34 +1,36 @@
 class InventoryItem:
 
     def __init__(self, price=0, days_last_changed=0):
-        self._last_price = price
         self._price = price
         self._days_since_price_changed = days_last_changed
-        self._days_since_last_price_changed = 0
         self._promotion_active = False
+        self._days_promotion_active = 0
 
     def set_price(self, price):
-        self._last_price = self._price
+        self._promotion_active = self.check_promotion(
+            self._price, 
+            price,
+            self._days_since_price_changed
+        )
+
         self._price = price
-        self._days_since_last_price_changed = self._days_since_price_changed
         self._days_since_price_changed = 0
 
-        self._promotion_active = self.check_promotion()
 
-    def price_drop_ratio(self):
-        return round(1 - (self._price / self._last_price), 2)
+    def price_drop_ratio(self, old_price, new_price):
+        return round(1 - (new_price / old_price), 2)
 
     def in_promotion(self):
         return self._promotion_active
 
-    def check_promotion(self):
-        if self._last_price == 0:
+    def check_promotion(self, old_price, new_price, days_last_changed):
+        if old_price == 0:
             return False
 
-        if self._days_since_last_price_changed < 30:
+        if days_last_changed < 30:
             return False
 
-        price_drop_ratio = self.price_drop_ratio()
+        price_drop_ratio = self.price_drop_ratio(old_price, new_price)
 
         if price_drop_ratio > 0.30:
             return False
--- tests/test_inventory_item.py
+++ tests/test_inventory_item.py
@@ -11,27 +11,20 @@ class TestInventoryItem(unittest.TestCase):
     def test_it_should_have_a_normal_price(self):
         self.subject._price.should.equal(0)
 
-    def test_it_should_have_a_last_price(self):
-        self.subject._last_price.should.equal(0)
-
     def test_it_should_not_be_in_promotion(self):
         self.subject.in_promotion().should.equal(False)
 
     def test_it_should_have_a_day_counter_since_price_changed(self):
         self.subject._days_since_price_changed.should.equal(0)
 
-    def test_it_should_have_a_day_counter_since_last_price_changed(self):
-        self.subject._days_since_last_price_changed.should.equal(0)
-
     def test_it_should_start_in_a_non_promotional_state(self):
         self.subject._promotion_active.should.equal(False)
+        self.subject._days_promotion_active.should.equal(0)
 
     def test_it_should_let_us_set_price_and_duration_on_init(self):
         self.other_subject = InventoryItem(25, 30)
         self.other_subject._price.should.equal(25)
         self.other_subject._days_since_price_changed.should.equal(30)
-        self.other_subject._last_price.should.equal(25)
-        self.other_subject._days_since_last_price_changed.should.equal(0)
 
     def test_it_should_let_us_set_a_new_price(self):
         self.subject.set_price(25)
@@ -48,17 +41,8 @@ class TestInventoryItemPriceChange(unittest.TestCase):
         self.subject.set_price(50)
         self.subject._days_since_price_changed.should.equal(0)
 
-    def test_it_should_update_last_price_changed_when_price_is_set(self):
-        self.subject.set_price(50)
-        self.subject._days_since_last_price_changed.should.equal(30)
-
-    def test_it_should_know_its_last_price_when_changed(self):
-        self.subject.set_price(50)
-        self.subject._last_price.should.equal(100)
-
     def test_it_should_calculate_price_drop_ratio(self):
-        self.subject.set_price(50)
-        self.subject.price_drop_ratio().should.equal(0.5)
+        self.subject.price_drop_ratio(100, 50).should.equal(0.5)
 
     def test_it_should_not_enter_promotion_when_price_drops_less_than_5_percent(self):
         self.subject.set_price(96)
